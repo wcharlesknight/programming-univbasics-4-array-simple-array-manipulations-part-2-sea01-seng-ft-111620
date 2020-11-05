0e8a5de5f292f84287df3876179a1763abdf1723
Charlie Knight <williamchknight@gmail.com>
2020-11-05 21:51:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,17 @@ end
 def using_insert(array, element)
   array.insert(4,element)
 end
+
+def using_uniq(array)
+  array.uniq(array)
+end
+
+def using_flatten(array)
+  array.flatten(array)
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+  
+
